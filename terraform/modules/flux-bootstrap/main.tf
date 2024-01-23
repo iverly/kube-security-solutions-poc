adaaf367608b2828fdc4f7abbdceb0685ec5f4cb
iverly <github@iverly.net>
2024-01-23 09:21:41 +0100

    feat(terraform): add flux bootstrap module
    Signed-off-by: iverly <github@iverly.net>

---
+++ terraform/modules/flux-bootstrap/main.tf
@@ -0,0 +1,59 @@
+resource "helm_release" "this" {
+  name = "flux2"
+
+  repository = "https://fluxcd-community.github.io/helm-charts"
+  chart      = "flux2"
+
+  namespace        = "flux-system"
+  create_namespace = true
+  wait             = true
+}
+
+resource "helm_release" "sync" {
+  name = "kube-security-solutions-poc"
+
+  repository = "https://fluxcd-community.github.io/helm-charts"
+  chart      = "flux2-sync"
+
+  namespace = "flux-system"
+  wait      = true
+
+  ## Git Repository
+  set {
+    name  = "gitRepository.spec.url"
+    value = "https://github.com/iverly/kube-security-solutions-poc"
+  }
+
+  set {
+    name  = "gitRepository.spec.interval"
+    value = "5m"
+  }
+
+  set {
+    name  = "gitRepository.spec.ref.branch"
+    value = "main"
+  }
+
+  ## Kustomization Repository
+  set {
+    name  = "kustomization.spec.interval"
+    value = "5m"
+  }
+
+  set {
+    name  = "kustomization.spec.prune"
+    value = "true"
+  }
+
+  set {
+    name  = "kustomization.spec.wait"
+    value = "true"
+  }
+
+  set {
+    name  = "kustomization.spec.path"
+    value = "infrastructure/flux"
+  }
+
+  depends_on = [helm_release.this]
+}
